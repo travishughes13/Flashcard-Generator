f33cda3858a2ad1f665292402820c65bb8afe391
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-05-21 10:00:28 -0500

    Everything works, console logs added to show completion

---
+++ flashcard.js
@@ -0,0 +1,60 @@
+// This section is for the variables to create the card
+var basic = '';
+var cloze = '';
+
+var basicCards = [];
+var clozeCards = [];
+
+// This section parses user input
+function basicMaker () {
+            for (var i = 3; i < process.argv.length; i++) {
+            basic = basic + process.argv[i] + ' ';
+            };
+        };
+
+function clozeMaker () {
+            for (var i = 3; i < process.argv.length; i++) {
+            cloze = cloze + process.argv[i] + ' ';
+            };
+        };
+
+// This section creates the cards
+function BasicCard(front, back) {
+        this.front = basicString[0];
+        this.back = basicString[1];
+};
+
+function ClozeCard(full, cloze, partial) {
+    this.full = full;
+    this.cloze = cloze;
+    this.partial = partial;
+};
+
+// This creates basic cards
+if (process.argv[2].toUpperCase() === 'BASIC') {
+    basicMaker();
+    basicString = basic.split('\? ');
+    var newBasicCard = new BasicCard();
+    basicCards.push(newBasicCard);
+    console.log('Front: ' + newBasicCard.front);
+    console.log('Back: ' + newBasicCard.back);
+}
+
+// This creates the cloze cards
+if (process.argv[2].toUpperCase() === 'CLOZE') {
+    clozeMaker();
+    clozeString = cloze.split('\. ');
+        if (clozeString.length < 2) {
+            console.log('Ya done stepped in it now, son');
+        }
+        else {
+            full = clozeString[0];
+            cloze = clozeString[1];
+            partial = full.split(cloze)[1];
+            var newClozeCard = new ClozeCard(full, cloze, partial);
+            clozeCards.push(newClozeCard);
+            console.log('Full: ' + newClozeCard.full);
+            console.log('Cloze: ' + newClozeCard.cloze);
+            console.log('Partial: ' + newClozeCard.partial);
+        }
+}
